dff0f471456de1b68fe566880f4e0c74bb5abdee
Cedric Geissmann <cedric.geissmann@gmail.com>
2025-07-21 13:14:58 +0200

    Use borders to separate layout parts.

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,6 @@ onMount( async () => {
 
 function pickColor(ev) {
     const lightness = getLightnessFromHey(ev.target.value)
-    console.log(lightness)
     const baseColors = `
         --base-color: ${ev.target.value};
         --text-color: ${lightness > 60 ? "black" : "white"}    
@@ -32,31 +31,58 @@ function getLightnessFromHey(hex) {
 
 </script>
 
-<header>Hello
-    <label for="color-picker">Base-Color:
+<header>
+    <h1>Volleybook</h1>
+    <label for="color-picker">Color:
         <input type="color" id="color-picker" on:change={pickColor} />
     </label>
 </header>
+
 <aside>some content</aside>
 <main>
-    <h1>Volleybook</h1>
+    
 </main>
 <footer>Footer</footer>
 
 <style>
 aside {
     grid-area: navbar;
-    background-color: var(--surface-color);
     height: 100%;
+    border-right: 2px solid var(--surface-color) ;
 }
 
 main {
     grid-area: main;
 }
+
 header {
     grid-area: header;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    border-bottom: 2px solid var(--surface-color) ;
 }
+
+label:has(#color-picker) {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 0.5rem;
+}
+
+#color-picker {
+    height: 20px;
+    width: 20px;
+    padding: 0;
+    border: var(--text-color) 1px solid;
+    border-radius: 50%;
+    margin: 0;
+}
+
 footer {
     grid-area: footer;
 }
+
 </style>
